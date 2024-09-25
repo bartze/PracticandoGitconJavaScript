831e8ae84badb12d11c8bc4fcff078ef238d2c2a
InakiB <inaki.barcelo@gmail.com>
2024-09-25 10:20:59 +0200

    Añado una segunda funcion para mirar si un número es positivo, negativo o cero

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function checkEqual(a, b) {
   return a == b ? `${a} is equal to ${b}` : `${a} is not equal to ${b}`;
 }
-
 console.log(checkEqual(1, 2));
 console.log(checkEqual(3, 3));
+
+function checkSign(num) {
+  return num === 0 ? "zero" : num > 0 ? "positive" : "negative";
+}
+checkSign(10);
+console.log(checkSign(10));
+console.log(checkSign(0));
+console.log(checkSign(-2));
