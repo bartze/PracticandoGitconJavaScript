c6d69ded5fbb9f427174747b760691812d0bd12c
InakiB <inaki.barcelo@gmail.com>
2024-09-25 09:51:35 +0200

    Añado console.logs al código js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function checkEqual(a, b) {
-  return a == b ? `${a} is equal to ${b}` : `${a} isn't equal to ${b}`;
+  return a == b ? `${a} is equal to ${b}` : `${a} is not equal to ${b}`;
 }
 
 console.log(checkEqual(1, 2));
+console.log(checkEqual(3, 3));
